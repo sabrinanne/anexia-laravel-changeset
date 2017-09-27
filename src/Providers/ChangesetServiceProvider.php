39f89e2e5011cb392158fb4948b7acf62a2801aa
Alexandra Bruckner <ABruckner@anexia-it.com>
2017-09-27 14:11:00 +0200

    load migrations from

--- src/Providers/ChangesetServiceProvider.php
+++ src/Providers/ChangesetServiceProvider.php
@@ -17,15 +17,7 @@ class ChangesetServiceProvider extends ServiceProvider
     public function boot()
     {
         // add additional migration files
-        $this->publishes([
-            __DIR__ . '/../../database/migrations/__create_change_records_table.php' => $this->app['path.database'] . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . '__create_change_records_table.php',
-        ], 'anexia-change-set');
-        $this->publishes([
-            __DIR__ . '/../../database/migrations/__create_change_sets_table.php' => $this->app['path.database'] . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . '__create_change_sets_table.php',
-        ], 'anexia-change-record');
-        $this->publishes([
-            __DIR__ . '/../../database/migrations/_create_change_set_foreign_table.php' => $this->app['path.database'] . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . '_create_change_set_foreign_table.php',
-        ], 'anexia-change-set-foreign-keys');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
 
     /**
